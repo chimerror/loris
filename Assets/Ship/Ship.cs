81167cd26dd37dcd77d4e24e4124d39f85f7aa38
Jason Mitchell <foobardog@hotmail.com>
2018-04-20 22:21:35 -0700

    Added Walls, Shrank Ship

--- Assets/Ship/Ship.cs
+++ Assets/Ship/Ship.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-    public float HorizontalSpeed = 2.5f;
-    public float VerticalSpeed = 2.5f;
+    public float HorizontalSpeed = 0.25f;
+    public float VerticalSpeed = 0.25f;
+    public float HorizontalWall = 10f;
+    public float VerticalWall = 10f;
 
 	private void Update ()
     {
         Vector3 positionVector = transform.position;
         positionVector.x += HorizontalSpeed * Input.GetAxis("Horizontal");
+        positionVector.x = Mathf.Clamp(positionVector.x, -HorizontalWall, HorizontalWall);
         positionVector.y += VerticalSpeed * Input.GetAxis("Vertical");
+        positionVector.y = Mathf.Clamp(positionVector.y, -VerticalWall, VerticalWall);
         transform.position = positionVector;
 	}
 }
