16245a5cc79868ad463174cb9c2cd06c2aeb9477
Jason Mitchell <foobardog@hotmail.com>
2018-04-21 16:31:46 -0700

    Shots now ignore provided tags

--- Assets/Shot/Shot.cs
+++ Assets/Shot/Shot.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Shot : MonoBehaviour
@@ -7,6 +8,7 @@ public class Shot : MonoBehaviour
     public float speed = .75f;
     public float offscreen = 4.25f;
     public float damage = 15f;
+    public List<string> tagsToIgnore = new List<string>();
 
 	private void Update()
     {
@@ -22,6 +24,11 @@ public class Shot : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (tagsToIgnore.Any(t => collision.gameObject.CompareTag(t)))
+        {
+            return;
+        }
+
         var damageable = collision.gameObject.GetComponent<Damageable>();
         if (damageable != null)
         {
