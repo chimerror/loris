89ee800df55a2325db8d569f895ce33e1df8b810
Jason Mitchell <foobardog@hotmail.com>
2018-04-20 22:04:59 -0700

    Added a ship with programmer art

---
+++ Assets/Ship/Ship.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float HorizontalSpeed = 2.5f;
+    public float VerticalSpeed = 2.5f;
+
+	private void Update ()
+    {
+        Vector3 positionVector = transform.position;
+        positionVector.x += HorizontalSpeed * Input.GetAxis("Horizontal");
+        positionVector.y += VerticalSpeed * Input.GetAxis("Vertical");
+        transform.position = positionVector;
+	}
+}
