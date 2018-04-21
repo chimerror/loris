8084cd61a66d542b10cd6b191d4aa116cb69e3b7
Jason Mitchell <foobardog@hotmail.com>
2018-04-21 00:02:38 -0700

    Added Damageable Asteroid

---
+++ Assets/Damageable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    public float hitPoints = 100;
+
+    private void Update()
+    {
+        if (hitPoints < 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Shot/Shot.cs
+++ Assets/Shot/Shot.cs
@@ -6,6 +6,7 @@ public class Shot : MonoBehaviour
 {
     public float speed = .75f;
     public float offscreen = 4.25f;
+    public float damage = 15f;
 
 	private void Update()
     {
@@ -18,4 +19,10 @@ public class Shot : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
         }
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var damageable = collision.collider.GetComponent<Damageable>();
+        damageable.hitPoints -= damage;
+    }
 }
