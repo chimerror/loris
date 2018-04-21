8acac0f28fb63ec07a179fdb6eae7697062477bb
Jason Mitchell <foobardog@hotmail.com>
2018-04-21 09:41:29 -0700

    Asteroids are spawned!

---
+++ Assets/AsteroidSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject asteroidPrefab = null;
+    public float horizontalRange = 9f;
+    public float spawnChance = 0.25f;
+
+    private void Update()
+    {
+        if (Random.Range(0f, 1f) <= spawnChance)
+        {
+            GameObject asteroid = Instantiate(asteroidPrefab, transform.position, Quaternion.identity);
+            float horizontalPosition = Random.Range(-horizontalRange, horizontalRange);
+            asteroid.transform.position =
+                new Vector3(horizontalPosition, asteroid.transform.position.y, asteroid.transform.position.z);
+            asteroid.gameObject.SetActive(true);
+        }
+    }
+
+}
