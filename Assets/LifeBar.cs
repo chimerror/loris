54881c1531b0847730ec0799fe87504c262273e6
Jason Mitchell <foobardog@hotmail.com>
2018-04-22 14:02:45 -0700

    Added Life Bar

---
+++ Assets/LifeBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(RectTransform))]
+public class LifeBar : MonoBehaviour
+{
+    public Ship ship;
+    public float barScale = 2f;
+
+    private RectTransform _rectTransform;
+    private Damageable _shipDamageable;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _shipDamageable = ship.GetComponent<Damageable>();
+    }
+
+    public void Update()
+    {
+        _rectTransform.sizeDelta = new Vector2(_shipDamageable.hitPoints * barScale, _rectTransform.sizeDelta.y);
+    }
+}
